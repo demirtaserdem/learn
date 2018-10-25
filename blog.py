cf9709522ddf4aea5840dcf81641a988d1e110f0
Erdem Demirtaş <demirtaserdem@gmail.com>
2018-10-25 11:14:13 +0300

    13. Flask Url Yapısı

--- blog.py
+++ blog.py
@@ -2,5 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Anasayfa"
+
+@app.route("/about")
+def about():
+    return "Hakkında"
+
+@app.route("/about/erdem")
+def erdem():
+    return "Erdem Hakkında"
+
 if __name__ == "__main__":
     app.run(debug = True)
