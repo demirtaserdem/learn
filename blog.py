043fb9f37d26eda2dfb50d27092ea6abf97ea9fb
Erdem Demirtaş <demirtaserdem@gmail.com>
2018-10-25 21:58:00 +0300

    25. Flask ile Mysql Konfigurasyonu

--- blog.py
+++ blog.py
@@ -4,8 +4,15 @@ from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
 
-
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "edblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
 
 @app.route("/")
 def index():
