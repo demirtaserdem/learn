18f3104c4fc45bcd5894f4114f8663de15bbbbfa
Erdem Demirtaş <demirtaserdem@gmail.com>
2018-10-25 12:33:44 +0300

    15. Template Inheritance ve Blocklar - Layoutun hepsini miras aldık.

--- blog.py
+++ blog.py
@@ -4,13 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    article = dict()
-    article["title"] = "deneme"
-    article["body"] = "Deneme123"
-    article["autor"] = "erdem demirtaş"
-    
-
-    return render_template("index.html",article = article)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
