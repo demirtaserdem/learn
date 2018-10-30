9618b4ebb8edc6d6cdc8f8b6d0c31d512d31ce7b
demirtaserdem <demirtaserdem@gmail.com>
2018-10-30 22:31:24 +0300

    62. Admin Panelini Özelleştirmek

--- djblog/article/admin.py
+++ djblog/article/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["title","author","created_date","content"]
+    list_display_links = ["title","created_date"]
+    search_fields = ["title"]
+    list_filter = ["created_date"]
+    class Meta:
+        model = Article
