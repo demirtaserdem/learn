b5ac936cf75a78453f4ed6814e6472ded92dc928
demirtaserdem <demirtaserdem@gmail.com>
2018-10-31 13:50:43 +0300

    69. Django Template İlişkisi - Döngüler ve Koşullar

--- djblog/article/views.py
+++ djblog/article/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
+    context = {
+        "numbers" : [1,2,3,4,5,6]
+    }
      #return HttpResponse("Anasayfa")
-     return render(request,"index.html")
-
+     #return render(request,"index.html",{"number":"1"})
+    return render(request,"index.html",context)
 def about(request):
     return render(request,"about.html")
