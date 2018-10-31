dd54c395b10e4569f0521773aa7282ef8267fa6b
demirtaserdem <demirtaserdem@gmail.com>
2018-10-31 13:15:55 +0300

    66. Template Inheritance ve Anasayfanın Oluşturulması

--- djblog/article/views.py
+++ djblog/article/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
      #return HttpResponse("Anasayfa")
-     return render(request,"article/index.html")
+     return render(request,"index.html")
