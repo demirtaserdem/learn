942c6722bc1070fe3d131173c91c8320f30a25e4
demirtaserdem <demirtaserdem@gmail.com>
2018-10-31 20:29:36 +0300

    74. Yeni Kullanıcı Kaydının Tamamlanması - 2. yöntem

--- djblog/user/views.py
+++ djblog/user/views.py
@@ -7,36 +7,24 @@ from django.contrib.auth import login
 # Create your views here.
 
 def register(request):
-    if request.method == "POST":
-        form =RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-
-            newUser = User(username = username)
-            newUser.set_password(password)
-            newUser.save()
-            login(request,newUser)
-            return redirect("index")
-        context = {
-            "form" : form
-        }
-        return render(request,"register.html",context)
-
-    else:
-        form = RegisterForm()
-        context = {
-            "form" : form
-        }
-        return render(request,"register.html",context)
     
-    """
-    form = RegisterForm()
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username = username)
+        newUser.set_password(password)
+        newUser.save()
+        login(request,newUser)
+        return redirect("index")
+    
     context = {
         "form" : form
     }
     return render(request,"register.html",context)
-    """
+  
+
 def loginUser(request):
     return render(request,"login.html")
 
