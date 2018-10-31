dcbcc45f8e24717e3896e68d507e390344290be5
demirtaserdem <demirtaserdem@gmail.com>
2018-10-31 13:36:19 +0300

    68. Hakkımızda Sayfası Oluşturma

--- djblog/djblog/urls.py
+++ djblog/djblog/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from article import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("",views.index,name = "index")
+    path ('',views.index,name = "index"),
+    path('about/',views.about,name = "about")
 ]
