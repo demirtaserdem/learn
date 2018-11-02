ba7e010ae05f9c2805a2d6a12b10fb8b490d27dc
demirtaserdem <demirtaserdem@gmail.com>
2018-11-02 17:29:36 +0300

    90. login_required

--- djblog/article/views.py
+++ djblog/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
 from .forms import ArticleForm
 from django.contrib import messages
 from .models import Article
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     context = {
@@ -15,6 +16,7 @@ def index(request):
 def about(request):
     return render(request,"about.html")
 
+@login_required(login_url = "user:login")
 def dashboard(request):
     articles = Article.objects.filter(author = request.user)
     context = {
@@ -22,6 +24,7 @@ def dashboard(request):
     }
     return render(request,"dashboard.html",context)
 
+@login_required(login_url = "user:login")
 def addArticle(request):
     form = ArticleForm(request.POST or None,request.FILES or None)
     if form.is_valid():
@@ -41,6 +44,7 @@ def detail(request,id):
     article = get_object_or_404(Article,id = id)
     return render(request,"detail.html",{"article":article})
 
+@login_required(login_url = "user:login")
 def update(request,id):
     article = get_object_or_404(Article,id = id)
     form =  ArticleForm(request.POST or None,request.FILES or None,instance=article)
@@ -52,6 +56,7 @@ def update(request,id):
         return redirect("article:dashboard")
     return render(request,"update.html",{"form":form})
 
+@login_required(login_url = "user:login")
 def deleteArticle(request,id):
     article = get_object_or_404(Article,id = id)
     article.delete()
