6eccbbb923aedfe1b55359c538799b025b01c223
demirtaserdem <demirtaserdem@gmail.com>
2018-11-03 21:39:44 +0300

    99. Yorumları ve Makaleleri Tarihe Göre Sıralama

--- djblog/article/models.py
+++ djblog/article/models.py
@@ -10,11 +10,15 @@ class Article(models.Model):
     article_image = models.FileField(blank = True, null = True,verbose_name="Makaleye Fotoğraf Ekleyin")
     def __str__(self):
         return self.title
+    class Meta:
+        ordering = ['-created_date']
 
 class Comment(models.Model):
     article = models.ForeignKey(Article , on_delete = models.CASCADE , verbose_name = "Makale" , related_name = "comments")
     comment_author = models.CharField(max_length = 50,verbose_name ="İsim")
     comment_content = models.CharField(max_length = 200,verbose_name = "Yorum")
-    comment_date = models.DateField(auto_now_add=True,verbose_name = "Yorum Tarihi")
+    comment_date = models.DateTimeField(auto_now_add=True,verbose_name = "Yorum Tarihi")
     def __str__(self):
         return self.comment_content
+    class Meta:
+        ordering = ['-comment_date']
